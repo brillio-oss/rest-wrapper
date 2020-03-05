f3e97ab21516829f06f41854c28d7384968f3ce3
Pavan Prakash <pavan.prakash.contractor@move.com>
2020-03-05 23:47:05 +0530

    change server

--- functions/graphql.js
+++ functions/graphql.js
@@ -1,14 +1,13 @@
 const ApolloServer = require("apollo-server-lambda");
 const { typeDefs } = require("./bundle/type_defs");
-const { resolvers } = require("./bundle/resolvers")
+const { resolvers } = require("./bundle/resolvers");
 
-const server = () =>
-  new ApolloServer({
-    typeDefs,
-    resolvers,
-    introspection: true,
-    playground: true
-  });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true
+});
 
 exports.handler = server.createHandler({
   cors: {
